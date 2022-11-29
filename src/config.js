b754b73ab3708d01602a710137c9c51f2c5db8c1
Erfan Enferad <erfanenferad@gmail.com>
2022-11-29 14:45:16 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
 export const API_HOST = 'http://localhost:8080/';
 
 //export const API_HOST = 'https://smartbuilding.cleverapps.io/';
+//export const API_HOST = 'https://smartbuilding.cleverapps.io/api';
